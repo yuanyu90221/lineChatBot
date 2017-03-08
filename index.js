c96ad48439844bfe51dfe9cb5f5c41de85b2594c
yuanyu90221 <yuanyu90221@gmail.com>
2017-03-08 15:26:05 +0800

    add getJson

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var pm = [];
 _getJSON();
 
 _bot();
-let bot = linebot({
+var bot = linebot({
   channelId: '1504696193',
   channelSecret: 'f866d3123893ea89749da78aa08f9844',
   channelAccessToken: 'bv6C//VSNXAOJ19xWX1kf91iq7t5VAMHO4b1PdGYQrF3sUBH2B+XEA8HzbaF7r4WoVPTFYmjOvq4wMiC1ySe+Xi7oBApYTQjsuiis4XvuplrpBQ2mAxvBAiOnuTF4TTKGLOBgtAGxpSnBOEvssfpmQdB04t89/1O/w1cDnyilFU='
@@ -21,7 +21,7 @@ const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-let server = app.listen(process.env.PORT || 8080, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
